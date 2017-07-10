a1d87311fa8526f88b0990af4fe3b07c8aff0302
Shane Yi <shane.yi@broadcom.com>
2017-07-10 14:20:01 -0700

    shmoo: v2.3.1.0 for 7268b0 and 7271b0
    NOTE: Samsumg DRAM parts are not supported with v2.3.1.0.
    
    v2.3.1.0 is an M1* (Ehm One Star) release, which is close to M1.
    More update(s) to Shmoo of 7268b0 and 7271b0 is expected as
    the limitation of support for Samsung parts gets resolved.
    
    SHA1(shmoo/B220_H00/lib/memsysinitlib.a)=
        1cf28960b66f37310ba4ad6d3f98bb427846cc9c
    
    from:
    http://confluence.broadcom.com/display/FWMEMSYS/Memsys+2.3.1.0
    
    MCB's have also been updated using those included in v2.3.0.1:
    http://confluence.broadcom.com/display/FWMEMSYS/Memsys+2.3.0.1
    
    refs #SWBOLT-2386
    
    Signed-off-by: Shane Yi <shane.yi@broadcom.com>

--- shmoo/B220_H00/mcb-7271b0.c
+++ shmoo/B220_H00/mcb-7271b0.c
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Broadcom Proprietary and Confidential. (c)2016 Broadcom. All rights reserved.
+ * Broadcom Proprietary and Confidential. (c)2017 Broadcom. All rights reserved.
  *
  *  THIS SOFTWARE MAY ONLY BE USED SUBJECT TO AN EXECUTED SOFTWARE LICENSE
  *  AGREEMENT  BETWEEN THE USER AND BROADCOM.  YOU HAVE NO RIGHT TO USE OR
@@ -18,10 +18,10 @@ static const uint32_t memsys_config_template[SHMOO_ARRAY_ELEMENTS] __attribute__
 	0x0132a6ef,	/* 0 */
 	0x00000000,	/* 1 */
 	0x00000000,	/* 2 */
-	0x1b2c1410,	/* 3 */
-	0x00010002,	/* 4 */
-	0x00001e02,	/* 5 */
-	0x00803d3c,	/* 6 */
+	0x451e4ffd,	/* 3 */
+	0x00020002,	/* 4 */
+	0x00002502,	/* 5 */
+	0x0020430d,	/* 6 */
 	0x007271b0,	/* 7 */
 	0x0000b220,	/* 8 */
 	0x00004800,	/* 9 */
@@ -37,22 +37,22 @@ static const uint32_t memsys_config_template[SHMOO_ARRAY_ELEMENTS] __attribute__
 	0x00000002,	/* 19 */
 	0x00002000,	/* 20 */
 	0x00002000,	/* 21 */
-	0x00486060,	/* 22 */
+	0x00404000,	/* 22 */
 	0x32323232,	/* 23 */
 	0x00001a1c,	/* 24 */
 	0x00001a1c,	/* 25 */
 	0x0000000d,	/* 26 */
 	0x03ff03ff,	/* 27 */
 	0x00000000,	/* 28 */
-	0x00216d5c,	/* 29 */
-	0x100a4400,	/* 30 */
-	0x00000010,	/* 31 */
-	0x00801400,	/* 32 */
+	0x00296d5c,	/* 29 */
+	0x100a5500,	/* 30 */
+	0x0000000a,	/* 31 */
+	0x00801500,	/* 32 */
 	0x00003c5a,	/* 33 */
-	0x00216d5c,	/* 34 */
-	0x500a4400,	/* 35 */
-	0x00000010,	/* 36 */
-	0x00801400,	/* 37 */
+	0x00296d5c,	/* 34 */
+	0x500a5500,	/* 35 */
+	0x0000000a,	/* 36 */
+	0x00801500,	/* 37 */
 	0x00003c5a,	/* 38 */
 	0x00000218,	/* 39 */
 	0x55010012,	/* 40 */
@@ -135,16 +135,16 @@ static const uint32_t memsys_config_template[SHMOO_ARRAY_ELEMENTS] __attribute__
 	0x00000404,	/* 117 */
 	0x0100016f,	/* 118 */
 	0x00006e6e,	/* 119 */
-	0x0618000c,	/* 120 */
+	0x3018000c,	/* 120 */
 	0x0000003c,	/* 121 */
 	0x00000028,	/* 122 */
 	0x0000003c,	/* 123 */
 	0xffffffff,	/* 124 */
-	0x00000003,	/* 125 */
-	0x02060600,	/* 126 */
-	0x04060e00,	/* 127 */
-	0x020c0600,	/* 128 */
-	0x00000000,	/* 129 */
+	0x000016f4,	/* 125 */
+	0x02140a00,	/* 126 */
+	0x04372000,	/* 127 */
+	0x02160400,	/* 128 */
+	0x00000030,	/* 129 */
 	0x1249a81c,	/* 130 */
 	0x844ec460,	/* 131 */
 	0x4820f31d,	/* 132 */
@@ -152,9 +152,9 @@ static const uint32_t memsys_config_template[SHMOO_ARRAY_ELEMENTS] __attribute__
 	0x1419018c,	/* 134 */
 	0x02df0430,	/* 135 */
 	0x00001868,	/* 136 */
-	0x00000000,	/* 137 */
-	0x00000000,	/* 138 */
-	0x00000000,	/* 139 */
+	0x0000000a,	/* 137 */
+	0x00000003,	/* 138 */
+	0x0000005a,	/* 139 */
 	0x00000000,	/* 140 */
 	0x00000000,	/* 141 */
 	0x00000000,	/* 142 */
@@ -172,22 +172,20 @@ static const uint32_t memsys_config_template[SHMOO_ARRAY_ELEMENTS] __attribute__
 /* 7271B_1600MHz_32b_dev8Gx16_LPDDR4-3200_le.mcb
 */
 static const uint32_t mcb_1600mhz_32b_dev8Gx16[] __attribute__ ((section(".mcbdata"))) = {
-	0xffffffff,  0x378acf0f,	/* -1 */
+	0xffffffff,  0x8b6f46e9,	/* -1 */
 }; /* 0 difference */
 
 /* 7271B_1867MHz_32b_dev8Gx16_LPDDR4-3733_le.mcb
 */
 static const uint32_t mcb_1867mhz_32b_dev8Gx16[] __attribute__ ((section(".mcbdata"))) = {
-	0x00000003,  0x7718bc60,	/* 3 */
-	0x00000005,  0x00002002,	/* 5 */
-	0x00000006,  0x00203fb9,	/* 6 */
+	0x00000003,  0xa1d31430,	/* 3 */
+	0x00000006,  0x0080430d,	/* 6 */
 	0x0000000f,  0x0000074b,	/* 15 */
 	0x00000010,  0x001c513f,	/* 16 */
-	0x00000016,  0x00404000,	/* 22 */
 	0x00000018,  0x00001e20,	/* 24 */
 	0x00000019,  0x00001e20,	/* 25 */
-	0x0000001d,  0x0021766c,	/* 29 */
-	0x00000022,  0x0021766c,	/* 34 */
+	0x0000001d,  0x0029766c,	/* 29 */
+	0x00000022,  0x0029766c,	/* 34 */
 	0x0000002a,  0x00101044,	/* 42 */
 	0x0000002b,  0x000bbcd8,	/* 43 */
 	0x0000002c,  0x00002d01,	/* 44 */
@@ -218,8 +216,8 @@ static const uint32_t mcb_1867mhz_32b_dev8Gx16[] __attribute__ ((section(".mcbda
 	0x00000086,  0x179d2dce,	/* 134 */
 	0x00000087,  0x03590438,	/* 135 */
 	0x00000088,  0x00001c78,	/* 136 */
-	0xffffffff,  0xef641faf,	/* -1 */
-}; /* 40 differences */
+	0xffffffff,  0x44d8cf4f,	/* -1 */
+}; /* 38 differences */
 
 const struct memsys_info __maybe_unused shmoo_data[] __attribute__ ((section(".mcbtable"))) = {
 	{ NULL, MCB_MAGIC1, MCB_MAGIC2, MCB_MAGIC3, -1, NULL },
